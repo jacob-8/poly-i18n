240c3a92676cdef981cbefffd022481066c57c4f
jacob-8 <jwrunner7@gmail.com>
2023-12-07 08:22:08 +0800

    add type

--- src/routes/[locale=locale]/+layout.ts
+++ src/routes/[locale=locale]/+layout.ts
@@ -4,6 +4,6 @@ import type { LocaleCode } from '$lib/poly-i18n/locales'
 
 export const load: LayoutLoad = async ({ params: { locale } }) => {
   const t = await getTranslator(locale as LocaleCode)
-  const i18n = await getDirectTranslator(locale)
+  const i18n = await getDirectTranslator(locale as LocaleCode)
   return { locale, t, i18n }
 }
